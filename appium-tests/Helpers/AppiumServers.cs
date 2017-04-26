3bdbb91b7f1b8b1b564245461c4e22149b2b390f
Hugo Campos <hcampos@totalproduce.com>
2017-04-26 16:43:10 +0100

    commit work done

---
+++ appium-tests/Helpers/AppiumServers.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium.Service.Options;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace appium_tests.Helpers
+{
+    class AppiumServers
+    {
+        private static AppiumLocalService LocalService;
+
+        public static Uri DriverURI = new Uri("http://127.0.0.1:4723/wd/hub");
+
+        public static Uri LocalServiceURIWindows
+        {
+            get
+            {
+                if (LocalService == null)
+                {
+                    AppiumServiceBuilder builder = new AppiumServiceBuilder().WithLogFile(new System.IO.FileInfo("Log"));
+                    LocalService = builder.Build();
+                }
+                if (LocalService.IsRunning)
+                {
+                    LocalService.Start();
+                }
+                return LocalService.ServiceUrl;
+            }
+        }
+
+        public static void StopLocalService()
+        {
+            if (LocalService != null && LocalService.IsRunning)
+            {
+                LocalService.Dispose();
+                LocalService = null;
+            }
+        }
+    }
+}
---
+++ appium-tests/PageObjects/StandardCalculatorScreen.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Interfaces;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Collections.Generic;
+
+namespace appium_tests.PageObjects
+{
+    class StandardCalculatorScreen
+    {
+        // Element locators
+        [FindsBy(How = How.Name, Using = "One")]
+        private IWebElement oneBtn;
+
+        [FindsBy(How = How.Name, Using = "Two")]
+        private IWebElement twoBtn;
+
+        [FindsBy(How = How.Name, Using = "Three")]
+        private IWebElement threeBtn;
+
+        [FindsBy(How = How.Name, Using = "Four")]
+        private IWebElement fourBtn;
+
+        [FindsBy(How = How.Name, Using = "Five")]
+        private IWebElement fiveBtn;
+
+        [FindsBy(How = How.Name, Using = "Six")]
+        private IWebElement sixBtn;
+
+        [FindsBy(How = How.Name, Using = "Seven")]
+        private IWebElement sevenBtn;
+
+        [FindsBy(How = How.Name, Using = "Eight")]
+        private IWebElement eightBtn;
+
+        [FindsBy(How = How.Name, Using = "Nine")]
+        private IWebElement nineBtn;
+
+        [FindsBy(How = How.Name, Using = "Zero")]
+        private IWebElement zeroBtn;
+
+        [FindsBy(How = How.Name, Using = "Plus")]
+        private IWebElement plusBtn;
+
+        [FindsBy(How = How.Name, Using = "Divide by")]
+        private IWebElement divideBtn;
+
+        [FindsBy(How = How.Name, Using = "Minus")]
+        private IWebElement minusBtn;
+
+        [FindsBy(How = How.Name, Using = "Multiply by")]
+        private IWebElement multiplyBtn;
+
+        [FindsBy(How = How.Name, Using = "Equals")]
+        private IWebElement equalsBtn;
+
+        [FindsBy(How = How.Name, Using = "Clear")]
+        private IWebElement clearBtn;
+    }
+}
